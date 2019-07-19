e3df9126ba21310a1d3dfb84516310623d899ffc
alex <alex_maie@yahoo.com>
2019-07-19 10:39:32 +0300

    watermark modified

--- addWatermark/addWatermark.php
+++ addWatermark/addWatermark.php
@@ -2,28 +2,28 @@
 
 function watermark($payLoad3)
 {
-    if(!isset($payLoad3['watermark']))
+    if(!isset($payLoad3[WATERMARK]))
         return $payLoad3;
 
-    $imageWaterMark = new Imagick($payLoad3['watermark']);
+    $imageWaterMark = new Imagick($payLoad3[WATERMARK]);
     $imageWaterMark->scaleImage($imageWaterMark->getImageWidth() / 10, 0);
 
-    $imageWidth = $payLoad3['image']->getImageWidth();
-    $imageHeight = $payLoad3['image']->getImageHeight();
+    $imageWidth = $payLoad3[IMAGE]->getImageWidth();
+    $imageHeight = $payLoad3[IMAGE]->getImageHeight();
     $waterMarkWidth = $imageWaterMark->getImageWidth();
     $waterMarkHeight = $imageWaterMark->getImageHeight();
 
     $coordinates = [
         [0,0],
         [$imageWidth - $waterMarkWidth, 0],
-        [0, $imageHeight - $waterMarkWidth],
+        [0, $imageHeight - $waterMarkHeight],
         [$imageWidth - $waterMarkWidth, $imageHeight - $waterMarkHeight]
     ];
 
     $waterMarkPosition = $coordinates[array_rand($coordinates)];
 
-    $payLoad3['image']->compositeImage($imageWaterMark,Imagick::COMPOSITE_OVER, $waterMarkPosition[0], $waterMarkPosition[1]);
-    unset($payLoad3['watermark']);
+    $payLoad3[IMAGE]->compositeImage($imageWaterMark,Imagick::COMPOSITE_OVER, $waterMarkPosition[0], $waterMarkPosition[1]);
+    unset($payLoad3[WATERMARK]);
 
     return $payLoad3;
 }
